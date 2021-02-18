6511230d47e875b9698ba32b6b04657cffa45311
Paul <paulfirmin1@gmail.com>
2021-02-18 03:33:59 +0000

    prepare for initial deployment

--- src/server.ts
+++ src/server.ts
@@ -31,14 +31,11 @@ app.use('/api/users', UserRouter);
 app.use('/api/folders', FolderRouter);
 app.use('/api/tasks', TaskRouter);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
-  app.get('*', (_req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
-    );
-  });
-}
+const root = path.join(__dirname, '..', 'client', 'build');
+app.use(express.static(root));
+app.get('*', (_req, res) => {
+  res.sendFile('index.html', { root });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
